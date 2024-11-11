064a476991be246bf3a0afa84e6a2a780009274e
Riri2601 <156933729+Riri2601@users.noreply.github.com>
2024-11-11 04:48:01 -0800

    Add files via upload

---
+++ OR_Portfolio_-main/script.js
@@ -0,0 +1,11 @@
+/* RESPONSIVE MOBILE VIEW NAVIGATION BAR*/
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
